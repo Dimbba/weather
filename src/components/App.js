ad9a5f5e38cfd64c7a54f0b6aac9a5a410cfb120
Timo Kannus <timokannus84@outlook.com>
2019-08-01 16:00:03 +0300

    Weather for all cities. Icons missing. Wind stats are correct but in one line. Temp in Kelvin.

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,20 @@
 import React from 'react';
-import axios from 'axios';
+import TRE from './TRE';
+import JKL from './JKL';
+import KUO from './KUO';
+import HKI from './HKI';
 
 class App extends React.Component {
-    state = {
-        city: undefined, //response.city.name,
-        country: undefined, //response.city.country,
-        temperature : undefined, //response.list[0].main.temp,
-        humidity: undefined, //response.list[0].main.humidity,
-        condition: undefined, //response.list[0].weather[0].description,
-        icon: undefined, //response.list[0].weather[0].icon,
-        wind: [undefined, undefined], //[response.list[0].wind.speed, response.list[0].wind.deg],
-        error: undefined,
-    }
-
-    componentDidMount() {
-        axios.get('http://api.openweathermap.org/data/2.5/group?id=634964,655195,650225,658225&units=metric&APPID=83d1900854a88677294589db185f3ded')
-        .then(response => response.data)
-        .then((data) => {
-            this.setState({
-                city: data.list[0].name,
-                temperature: data.list[0].main.temp,
-                humidity: data.list[0].main.humidity,
-                condition: data.list[0].weather[0].description,
-                icon: data.list[0].weather[0].icon,
-                wind: [data.list[0].wind.speed, data.list[0].wind.deg]
-            })
-            console.log(data);
-        })
-        .catch(error =>{
-            console.log(error);
-        });
-    }
-
     render() {
         return(
             <div>
-                <h2>Säätiedot</h2>
-                <div>
-                    <h3> Kaupunki: {this.state.city} </h3>
-                    <p> Lämpötila: {this.state.temperature} </p>
-                    <p> Ilmankosteus: {this.state.humidity} </p>
-                    <p> Kuvaus: {this.state.condition} </p>
-                    <i> Ikoni: {this.state.icon} </i>
-                    <p> Tuuli: {this.state.wind} </p>
-                </div>
+                <TRE />
+                <br />
+                <JKL />
+                <br />
+                <KUO />
+                <br />
+                <HKI />
             </div>
         );
     }
---
+++ src/components/HKI.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+
+class HKI extends React.Component {
+    state = {
+        city: undefined, //response.city.name,
+        country: undefined, //response.city.country,
+        temperature : undefined, //response.list[0].main.temp,
+        humidity: undefined, //response.list[0].main.humidity,
+        condition: undefined, //response.list[0].weather[0].description,
+        icon: undefined, //response.list[0].weather[0].icon,
+        wind: [undefined, undefined], //[response.list[0].wind.speed, response.list[0].wind.deg],
+        error: undefined,
+    }
+
+    componentDidMount() {
+        axios.get('http://api.openweathermap.org/data/2.5/weather?id=658225&APPID=83d1900854a88677294589db185f3ded')
+        .then(response => response.data)
+        .then((data) => {
+            this.setState({
+                city: data.name,
+                temperature: data.main.temp,
+                humidity: data.main.humidity,
+                condition: data.weather[0].description,
+                icon: data.weather.icon,
+                wind: [data.wind.speed, data.wind.deg]
+            })
+            console.log(data);
+        })
+        .catch(error =>{
+            console.log(error);
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <h2>Säätiedot</h2>
+                <div>
+                    <h3> Kaupunki: {this.state.city} </h3>
+                    <p> Lämpötila: {this.state.temperature} K</p>
+                    <p> Ilmankosteus: {this.state.humidity} </p>
+                    <p> Kuvaus: {this.state.condition} </p>
+                    <i> Ikoni: {this.state.icon} </i>
+                    <p> Tuuli: {this.state.wind} </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HKI;
