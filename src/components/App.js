0b94c45960bde3d54e75bde494073983c55fe5ba
Timo Kannus <timokannus84@outlook.com>
2019-08-01 14:25:13 +0300

    API call added. Console logging the response

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+    componentDidMount() {
+        axios.get('http://api.openweathermap.org/data/2.5/group?id=634964,655195,650225,658225&units=metric&APPID=83d1900854a88677294589db185f3ded')
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error =>{
+            console.log(error);
+        });
+    }
+
     render() {
-        return <div>Weather App</div>
+        return(
+            <div>
+                Weather
+            </div>
+        );
     }
 }
 
